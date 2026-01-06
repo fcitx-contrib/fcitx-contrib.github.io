24171a1916affe19b9b24332b5addd0e09cf0961
Qijia Liu <liumeo@pku.edu.cn>
2026-01-06 00:00:25 -0500

    unicode

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -47,6 +47,7 @@ export default defineConfig({
           { text: '剪贴板', link: '/docs/advanced/clipboard' },
           { text: '云拼音', link: '/docs/advanced/cloudpinyin'},
           { text: '简繁转换', link: '/docs/advanced/chttrans' },
+          { text: 'Unicode', link: '/docs/advanced/unicode' },
         ]
       },
       {
---
+++ docs/advanced/unicode.md
@@ -0,0 +1,12 @@
+# Unicode
+查询 Unicode 编码或输入 Unicode 字符。
+
+## 设置
+
+### 触发键
+按下 `Ctrl+Option+Shift+U` 时，
+* 若主选区或剪贴板有内容，将逐字显示 Unicode 编码和描述；
+* 输入 Unicode 字符的描述，或 `U+十六进制数字`，匹配的字符会作为候选字出现，选字键为 `Option+数字`。
+
+### 使用十六进制数字输入 Unicode 字符
+按下 `Ctrl+Shift+U` 时，直接输入十六进制数字（不需要先导 `U+`），空格上屏对应的 Unicode 字符。
